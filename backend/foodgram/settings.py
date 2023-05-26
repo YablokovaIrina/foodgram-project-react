d540ffde10c184b3224b47fffb2a6955e38f0172
Yablokova Irina <yablokovairinav@yandex.ru>
2023-05-26 23:10:04 +0300

    сетингс

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -146,8 +146,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
     ],
 }
 
