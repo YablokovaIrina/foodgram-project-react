779aef1c9f9e9eb785abd3d62d4ee7c176450766
Yablokova Irina <yablokovairinav@yandex.ru>
2023-05-27 21:20:51 +0300

    Надежда 5

--- backend/api/views.py
+++ backend/api/views.py
@@ -116,38 +116,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return RecipeSerializer
         return RecipeWriteSerializer
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        serializer = RecipeSerializer(
-            instance=serializer.instance,
-            context={'request': self.request}
-        )
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED
-        )
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(
-            instance,
-            data=request.data,
-            partial=True
-        )
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        serializer = RecipeSerializer(
-            instance=serializer.instance,
-            context={'request': self.request}
-        )
-        return Response(
-            serializer.data, status=status.HTTP_200_OK
-        )
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
 
 class FavouriteViewSet(
     mixins.CreateModelMixin,
