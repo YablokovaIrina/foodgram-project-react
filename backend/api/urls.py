e592db3c06228ef7b9d4ab73d913f69a02032a59
Yablokova Irina <yablokovairinav@yandex.ru>
2023-05-27 18:51:01 +0300

    Магия flake8

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (FavouriteViewSet, FollowGetViewSet, FollowViewSet,
-                    IngredientViewSet, RecipeViewSet, ShoppingCartViewSet,
+from .views import (FavouriteViewSet, IngredientViewSet,
+                    RecipeViewSet, ShoppingCartViewSet,
                     ShoppingListDownload, TagViewSet, UsersViewSet,)
 
 router = DefaultRouter()
